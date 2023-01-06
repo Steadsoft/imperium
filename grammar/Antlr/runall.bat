e298793c1bc398a7c453156168d36b2405b7d97a
Hugh <hugh_gleaves@hotmail.com>
2023-01-06 14:52:54 -0700

    Revert to the working earlier version of these files.

--- grammar/Antlr/runall.bat
+++ grammar/Antlr/runall.bat
@@ -1,4 +1,4 @@
-java org.antlr.v4.Tool ImperiumKeywords.g4 ImperiumParser.g4 -visitor
-java org.antlr.v4.Tool ImperiumKeywords.g4 ImperiumParser.g4 -visitor -Dlanguage=CSharp
+java org.antlr.v4.Tool Imperium.g4 -visitor
+java org.antlr.v4.Tool Imperium.g4 -visitor -Dlanguage=CSharp
 javac *.java
 java org.antlr.v4.gui.TestRig Imperium translation_unit test_3.ipl -gui 
