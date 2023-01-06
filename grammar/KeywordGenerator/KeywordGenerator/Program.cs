47dfad02a67ce18320aa8c26f2b3445d760d1514
Hugh <hugh_gleaves@hotmail.com>
2023-01-06 10:32:21 -0700

    Start a small app that will generate an Antlr lexer file from the json keywords file.

---
+++ grammar/KeywordGenerator/KeywordGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace KeywordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // load the dictionary.
+
+            var language_dictionary = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(@"..\..\..\..\..\antlr\imperium.keywords.json"));//.ToDictionary(x => x.Key, x => x.Value.ToDictionary(x => x.Value, x => x.Key));
+
+            Dictionary<string,string> output = new Dictionary<string,string>();
+
+            foreach (var language in language_dictionary)
+            {
+                foreach (var keyword_pair in language_dictionary[language.Key])
+                {
+                    if (!output.ContainsKey(keyword_pair.Key))
+                    {
+                        output[keyword_pair.Key] = $"{keyword_pair.Value} {language.Key}";
+                    }
+                    else
+                    {
+                        output[keyword_pair.Key] = output[keyword_pair.Key] + $" {keyword_pair.Value} {language.Key}";
+                    }
+                }
+            }
+
+        }
+    }
+}
