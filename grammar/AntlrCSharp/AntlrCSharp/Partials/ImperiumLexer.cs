078c881c7b90a6e343feaab9f8f51ef6e53ed911
Hugh <hugh_gleaves@hotmail.com>
2023-01-11 13:00:28 -0700

    Add support code so that generated parser code for Java can compiler as C#.

---
+++ grammar/AntlrCSharp/AntlrCSharp/Partials/ImperiumLexer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public partial class ImperiumLexer
+    {
+        public string getText()
+        {
+            return this.Text;
+        }
+
+        public void setText(string text)
+        {
+            Text = text;
+        }
+    }
+
---
+++ grammar/AntlrCSharp/AntlrCSharp/Statics/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public static class StringExtensions
+    {
+        public static StringBuilder append (this StringBuilder arg,string text)
+        {
+            return arg.Append (text);
+        }
+
+    public static String reverse(this StringBuilder arg)
+    {
+        return arg.ToString().ToCharArray().Reverse().ToString();
+    }
+
+    public static String toString(this StringBuilder arg)
+    {
+        return arg.ToString();
+    }
+
+}
+
+
+
