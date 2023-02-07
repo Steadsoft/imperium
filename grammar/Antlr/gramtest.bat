b9a9c4833f1f39b1eca8a745328af551ce0840bc
Hugh <hugh_gleaves@hotmail.com>
2023-02-07 16:38:28 -0700

    Grammar tidy up.

--- grammar/Antlr/gramtest.bat
+++ grammar/Antlr/gramtest.bat
@@ -1,4 +1,4 @@
 java org.antlr.v4.Tool Imperium.g4 -visitor
 java org.antlr.v4.Tool Imperium.g4 -visitor -Dlanguage=CSharp
 javac *.java
-java org.antlr.v4.gui.TestRig Imperium translation_unit grammar_test.ipl -gui -tokens
+java org.antlr.v4.gui.TestRig Imperium translationUnit grammar_test.ipl -gui -tokens
