3f1745e9b670a4e7007e8e7bd61b471c57d41372
Hugh <hugh_gleaves@hotmail.com>
2025-06-06 08:45:56 -0700

    Add the previously created support files used when exploring initial forays into AST creation.

---
+++ grammar/Antlr/Imperium/VisitorContext.cs
@@ -0,0 +1,50 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// SEE: https://stackoverflow.com/questions/29971097/how-to-create-ast-with-antlr4
+
+public class VisitorContext : ParserRuleContext
+{
+    public AstNode Node { get; set; }
+    public VisitorContext(ParserRuleContext P, int S) : base(P, S)
+    {
+
+    }
+
+    public VisitorContext() : base()
+    {
+
+    }
+}
+
+public class AstNode
+{
+    public List<AstNode> Children { get; set; }
+    public int Line { get; private set; }
+    public AstNode(int Line)
+    {
+        this.Line = Line;
+        this.Children = new List<AstNode>();
+    }
+}
+
+
+public partial class ImperiumLexer
+{
+    public string getText()
+    {
+        return this.Text;
+    }
+
+    public void setText(string text)
+    {
+        Text = text;
+    }
+}
+
+
+
